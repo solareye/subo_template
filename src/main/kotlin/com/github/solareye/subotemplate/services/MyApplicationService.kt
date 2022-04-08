4e39f3961c5b91532c61884c909e33840774be6a
GitHub Action <action@github.com>
2022-04-08 14:00:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/solareye/subotemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/solareye/subotemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.solareye.subotemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.solareye.subotemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/solareye/subotemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/solareye/subotemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.solareye.subotemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.solareye.subotemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
